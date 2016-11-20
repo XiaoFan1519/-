670a4a53115aef049ba01c3a5fd41424030c7e4a
XiaoFan1519 <xiaofanms@outlook.com>
2016-11-20 22:42:16 +0800

    开始将 GDI 替换成 D2D。

--- 这是我见过最无聊的游戏/main.cpp
+++ 这是我见过最无聊的游戏/main.cpp
@@ -1,13 +1,11 @@
-#include <windows.h>
-#include <vector>
-#include <queue>
+#include "init.h"
 #include "Circle.h"
 
 
 using namespace std;
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-void DrawCircle(HWND);
+LRESULT CALLBACK WndProc (HWND, UINT, WPARAM, LPARAM);
+void DrawCircle (HWND);
 
 // 保存圆的集合
 vector<Circle*> circles;
@@ -22,14 +20,22 @@ const ULONGLONG FPS_60 = 17;
 // 保存鼠标坐标
 POINT cursorPos;
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+// d2d 需要的变量
+ID2D1Factory* m_pDirect2dFactory;
+ID2D1HwndRenderTarget* m_pRenderTarget;
+
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	PSTR szCmdLine, int iCmdShow)
 {
+	// 系统初始化
+	HRESULT hr = CoInitialize (NULL);
+	assert (hr);
+
 	// 初始化容器
 	circles.reserve (10000);
 
 	// 初始化资源
-	
+
 
 	//初始化个圆
 	RECT rect;
@@ -37,12 +43,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	rect.top = 0;
 	rect.right = 800;
 	rect.bottom = 800;
-	COLORREF color = RGB(rand() % 256, rand() % 256, rand() % 256);
-	Circle* circle = new Circle(rect,RECT() ,color, 0, TRUE,400);
-	circles.push_back(circle);
+	COLORREF color = RGB (rand () % 256, rand () % 256, rand () % 256);
+	Circle* circle = new Circle (rect, RECT (), color, 0, TRUE, 400);
+	circles.push_back (circle);
 
 	//下面都是创建窗口的代码
-	static TCHAR szAppName[] = TEXT("CircleGame");
+	static TCHAR szAppName[] = TEXT ("CircleGame");
 	HWND hwnd;
 	MSG msg;
 	WNDCLASS wndclass;
@@ -51,38 +57,49 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	wndclass.cbClsExtra = 0;
 	wndclass.cbWndExtra = 0;
 	wndclass.hInstance = hInstance;
-	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hIcon = LoadIcon (NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor (NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject (BLACK_BRUSH);
 	wndclass.lpszMenuName = NULL;
 	wndclass.lpszClassName = szAppName;
-	if (!RegisterClass(&wndclass))
+	if (!RegisterClass (&wndclass))
 	{
-		MessageBox(NULL, TEXT("This program requires Windows NT!"),szAppName, MB_ICONERROR);
+		MessageBox (NULL, TEXT ("This program requires Windows NT!"), szAppName, MB_ICONERROR);
 		return 0;
 	}
-	hwnd = CreateWindow(szAppName, TEXT("这是我见过的最无聊的游戏"),WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX & ~WS_THICKFRAME,0, 0,800+16, 800+38,NULL, NULL, hInstance, NULL);
-	ShowWindow(hwnd, iCmdShow);
+	hwnd = CreateWindow (szAppName, TEXT ("这是我见过的最无聊的游戏"), WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX & ~WS_THICKFRAME, 0, 0, 800 + 16, 800 + 38, NULL, NULL, hInstance, NULL);
+	ShowWindow (hwnd, iCmdShow);
+	UpdateWindow (hwnd);
 
-	//获取客户区大小
-	GetClientRect(hwnd, &rect);
+	// 创建d2d资源
+	hr = S_OK;
+	// Create a Direct2D factory.
+	hr = D2D1CreateFactory (D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pDirect2dFactory);
+	assert (hr);
 
-	UpdateWindow(hwnd);
+	D2D1_SIZE_U size = D2D1::SizeU (816, 838);
 
-	do{
+	// Create a Direct2D render target.
+	hr = m_pDirect2dFactory->CreateHwndRenderTarget (
+		D2D1::RenderTargetProperties (),
+		D2D1::HwndRenderTargetProperties (hwnd, size),
+		&m_pRenderTarget
+	);
+
+	do {
 		//利用空闲时间来画画
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		if (PeekMessage (&msg, NULL, 0, 0, PM_REMOVE))
 		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
+			TranslateMessage (&msg);
+			DispatchMessage (&msg);
 		}
-		
-		DrawCircle(hwnd);
-	
-	}while (msg.message != WM_QUIT);
+
+		DrawCircle (hwnd);
+
+	} while (msg.message != WM_QUIT);
 
 	//删除所有的圆的指针
-	for (auto i = circles.begin(); i != circles.end(); ++i)
+	for (auto i = circles.begin (); i != circles.end (); ++i)
 	{
 		if (nullptr == *i)
 		{
@@ -91,24 +108,31 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		delete *i;
 		*i = NULL;
 	}
-	circles.clear();
+	circles.clear ();
+
+
+	// 释放 COM
+	SafeRelease (&m_pDirect2dFactory);
+	SafeRelease (&m_pRenderTarget);
+	
+	CoUninitialize ();
 
 	return msg.wParam;
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc (HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (iMsg)
 	{
 	case WM_DESTROY:
-		PostQuitMessage(0);
+		PostQuitMessage (0);
 		return 0;
 	case WM_MOUSEMOVE:
 		cursorPos.x = LOWORD (lParam);
 		cursorPos.y = HIWORD (lParam);
 		return 0;
 	}
-	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+	return DefWindowProc (hwnd, iMsg, wParam, lParam);
 }
 
 // 增加圆到容器中
@@ -126,7 +150,7 @@ void AddCircles (Circle* c)
 }
 
 // 增加四个圆
-void AddCircle(Circle* c){
+void AddCircle (Circle* c) {
 	RECT rTmp;
 	Circle *cTmp;
 	int len;
@@ -138,32 +162,32 @@ void AddCircle(Circle* c){
 	rTmp.top = c->m_location.top;
 	rTmp.right = c->m_location.right - len;
 	rTmp.bottom = c->m_location.bottom - len;
-	cTmp = new Circle(rTmp, c->m_location, c->m_Color, 0,FALSE,len);
+	cTmp = new Circle (rTmp, c->m_location, c->m_Color, 0, FALSE, len);
 	AddCircles (cTmp);
 
 	rTmp.left = c->m_location.left + len;
 	rTmp.top = c->m_location.top;
 	rTmp.right = c->m_location.right;
 	rTmp.bottom = c->m_location.bottom - len;
-	cTmp = new Circle(rTmp, c->m_location, c->m_Color, 1, FALSE, len);
+	cTmp = new Circle (rTmp, c->m_location, c->m_Color, 1, FALSE, len);
 	AddCircles (cTmp);
 
 	rTmp.left = c->m_location.left;
 	rTmp.top = c->m_location.top + len;
 	rTmp.right = c->m_location.right - len;
 	rTmp.bottom = c->m_location.bottom;
-	cTmp = new Circle(rTmp, c->m_location, c->m_Color, 2, FALSE, len);
+	cTmp = new Circle (rTmp, c->m_location, c->m_Color, 2, FALSE, len);
 	AddCircles (cTmp);
 
 	rTmp.left = c->m_location.left + len;
 	rTmp.top = c->m_location.top + len;
 	rTmp.right = c->m_location.right;
 	rTmp.bottom = c->m_location.bottom;
-	cTmp = new Circle(rTmp, c->m_location, c->m_Color, 3, FALSE, len);
+	cTmp = new Circle (rTmp, c->m_location, c->m_Color, 3, FALSE, len);
 	AddCircles (cTmp);
 }
 
-void DrawCircle(HWND hWnd)
+void DrawCircle (HWND hWnd)
 {
 	ULONGLONG start_time = GetTickCount64 ();
 	ULONGLONG end_time;
@@ -173,13 +197,13 @@ void DrawCircle(HWND hWnd)
 	HBITMAP hBit;
 	HPEN hPen;
 
-	hdc = GetDC(hWnd);
-	hmdc = CreateCompatibleDC(hdc);
+	hdc = GetDC (hWnd);
+	hmdc = CreateCompatibleDC (hdc);
 	hBit = CreateCompatibleBitmap (hdc, 800, 800);
 	// 透明画笔
 	hPen = CreatePen (PS_NULL, 0, 0);
-	auto oldPen = SelectObject(hmdc, hPen);
-	auto oldBit = SelectObject(hmdc, hBit);
+	auto oldPen = SelectObject (hmdc, hPen);
+	auto oldBit = SelectObject (hmdc, hBit);
 
 	int count = circles.size ();
 	for (int index = 0; index < count; ++index)
@@ -190,9 +214,9 @@ void DrawCircle(HWND hWnd)
 			continue;
 		}
 
-		if (c->InCircle(cursorPos))
+		if (c->InCircle (cursorPos))
 		{
-			AddCircle(c);
+			AddCircle (c);
 			//别忘了删!!!
 			delete c;
 			circles[index] = nullptr;
@@ -200,21 +224,21 @@ void DrawCircle(HWND hWnd)
 			continue;
 		}
 
-		c->IsOk();
-		c->Paint(hmdc);
+		c->IsOk ();
+		c->Paint (hmdc);
 	}
 
-	BitBlt(hdc, 0, 0, 800, 800, hmdc, 0, 0, SRCCOPY);
+	BitBlt (hdc, 0, 0, 800, 800, hmdc, 0, 0, SRCCOPY);
 
 	//注意释放顺序
-	SelectObject(hmdc, oldBit);
-	SelectObject(hmdc, oldPen);
+	SelectObject (hmdc, oldBit);
+	SelectObject (hmdc, oldPen);
 
 	// 释放资源
-	DeleteObject(hBit);
+	DeleteObject (hBit);
 	DeleteObject (hPen);
-	DeleteDC(hmdc);
-	ReleaseDC(hWnd, hdc);
+	DeleteDC (hmdc);
+	ReleaseDC (hWnd, hdc);
 
 	end_time = GetTickCount64 ();
 	ULONGLONG cost = end_time - start_time;
